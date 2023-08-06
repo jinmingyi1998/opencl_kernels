a5767990163f4021d842cedaa02610e7b161655e
Mingyi Jin <jinmingyi1998@sina.cn>
2023-08-07 00:00:52 +0800

    build(build): update build script, update setup.py

--- build_wheels.mk
+++ build_wheels.mk
@@ -1,38 +1,14 @@
 # run in docker quay.io/pypa/manylinux_2_28_x86_64:latest
-.PHONY: all  build_all clean cp36 cp37 cp38 cp39 cp310 cp311 cp312
+.PHONY: all clean
 
-all: build_all
-	/opt/python/cp36-cp36m/bin/python3 setup.py build bdist_wheel
-	/opt/python/cp37-cp37m/bin/python3 setup.py build bdist_wheel
-	/opt/python/cp38-cp38/bin/python3 setup.py build bdist_wheel
-	/opt/python/cp39-cp39/bin/python3 setup.py build bdist_wheel
-	/opt/python/cp310-cp310/bin/python3 setup.py build bdist_wheel
-	/opt/python/cp311-cp311/bin/python3 setup.py build bdist_wheel
-	/opt/python/cp312-cp312/bin/python3 setup.py build bdist_wheel
-
-build_all: cp36 cp37 cp38 cp39 cp310 cp311 cp312
-
-
-cp36:
-	/opt/python/cp36-cp36m/bin/python3 setup.py build
-
-cp37:
-	/opt/python/cp37-cp37m/bin/python3 setup.py build
-
-cp38:
-	/opt/python/cp38-cp38/bin/python3 setup.py build
-
-cp39:
-	/opt/python/cp39-cp39/bin/python3 setup.py build
-
-cp310:
-	/opt/python/cp310-cp310/bin/python3 setup.py build
-
-cp311:
-	/opt/python/cp311-cp311/bin/python3 setup.py build
-
-cp312:
-	/opt/python/cp312-cp312/bin/python3 setup.py build
+all:
+	/opt/python/cp36-cp36m/bin/python3  -m build -w -n .
+	/opt/python/cp37-cp37m/bin/python3  -m build -w -n .
+	/opt/python/cp38-cp38/bin/python3   -m build -w -n .
+	/opt/python/cp39-cp39/bin/python3   -m build -w -n .
+	/opt/python/cp310-cp310/bin/python3 -m build -w -n .
+	/opt/python/cp311-cp311/bin/python3 -m build -w -n .
+	/opt/python/cp312-cp312/bin/python3 -m build -w -n .
 
 clean:
 	rm -rf build/
