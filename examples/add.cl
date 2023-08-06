32b764d70f762debb4c59eee8fc7b7953c614883
Mingyi Jin <jinmingyi1998@sina.cn>
2023-08-06 23:10:30 +0800

    feat(example): update example with OOP Style and add kernel

---
+++ examples/add.cl
@@ -0,0 +1,24 @@
+kernel void
+add(global float *a, global float *b, long length, global float *out) {
+    long x = get_global_id(0);
+    if (x < length) {
+        out[x] = a[x] + b[x];
+    }
+}
+
+kernel void add_constant(global float *a, float x, long length,global float*out) {
+    long i = get_global_id(0);
+    if (i < length) {
+        out[i] = a[i] + x;
+    }
+}
+
+#ifndef BATCH_SIZE
+#define BATCH_SIZE 4
+#endif
+kernel void add_batch(global float *a, global float *b, long length, global float *out) {
+    long x = get_global_id(0)*BATCH_SIZE;
+    for (int i=0;i<BATCH_SIZE && x < length;i++,x++) {
+        out[x] = a[x] + b[x];
+    }
+}
---
+++ examples/add.py
@@ -0,0 +1,109 @@
+from typing import Dict, List, Union
+
+import numpy as np
+
+from oclk import Runner, TimerArgs
+
+
+def wrap_args(**kwargs) -> List[Dict[str, Union[str, np.ndarray, int, float]]]:
+    """
+    easily make the arg dict
+    """
+    return [{"name": k, "value": v} for k, v in kwargs.items()]
+
+
+def add():
+    r = Runner()
+    a = np.random.random([64, 64, 64, 64])
+    a = np.ascontiguousarray(a, dtype=np.float32)
+    b = np.random.random([64, 64, 64, 64])
+    b = np.ascontiguousarray(b, dtype=np.float32)
+
+    out = np.zeros_like(a)
+    out = np.ascontiguousarray(out, dtype=np.float32)
+
+    arr_length = a.size
+
+    r.run(
+        kernel_name="add",
+        input=[
+            {"name": "a", "value": a},
+            {"name": "b", "value": b},
+            {"name": "length", "value": arr_length},
+            {"name": "out", "value": out},
+        ],
+        output=["out"],
+        local_work_size=[1],
+        global_work_size=[arr_length],
+    )
+
+    c = np.float32(a + b)
+    print(np.abs(c - out).max())
+
+
+def add_constant():
+    r = Runner()
+
+    a = np.random.random([1, 2, 3, 4, 5])
+    x = 12.3
+
+    a = np.ascontiguousarray(a, dtype=np.float32)
+    arr_length = a.size
+    out = np.zeros_like(a)
+    out = np.ascontiguousarray(out, dtype=np.float32)
+
+    timer = TimerArgs(True, 1, 10, "add_constant")
+    r.run(
+        kernel_name="add_constant",
+        input=wrap_args(a=a, x=x, length=arr_length, out=out),
+        output=["out"],
+        local_work_size=[1],
+        global_work_size=[arr_length],
+        wait=True,
+        timer=timer,
+    )
+
+    b = np.float32(a + x)
+    print(np.abs(out - b).max())
+
+
+def add_batch():
+    r = Runner()
+
+    a = np.random.random([1, 2, 3, 4, 5])
+    b = np.random.random([1, 2, 3, 4, 5])
+    a = np.ascontiguousarray(a, dtype=np.float32)
+    b = np.ascontiguousarray(b, dtype=np.float32)
+    out = np.zeros_like(a)
+    out = np.ascontiguousarray(out, dtype=np.float32)
+    arr_length = a.size
+    timer = TimerArgs(True, 10, 100, "add_batch")
+    r.run(
+        kernel_name="add_batch",
+        input=wrap_args(a=a, b=b, length=arr_length, out=out),
+        output=["out"],
+        local_work_size=[1],
+        global_work_size=[arr_length // 4],
+        wait=True,
+        timer=timer,
+    )
+
+    a = np.float32(a)
+    b = np.float32(b)
+    c = a + b
+    c = np.float32(c)
+    print(np.abs(out - c).max())
+
+
+def main():
+    r = Runner()  # first init
+    r.load_kernel("add.cl", ["add", "add_constant"])
+    r.load_kernel("add.cl", "add_batch", "-DBATCH_SIZE=4")
+
+    add()
+    add_constant()
+    add_batch()
+
+
+if __name__ == "__main__":
+    main()
